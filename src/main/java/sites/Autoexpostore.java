00996e54986b24784f5347b1f01af17529ef4559
Andrey Bzhestovskyy <bzhest@gmail.com>
2017-07-27 17:37:14 +0300

    add 2 tests

--- src/main/java/sites/Autoexpostore.java
+++ src/main/java/sites/Autoexpostore.java
@@ -8,7 +8,10 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -39,36 +42,54 @@ public class Autoexpostore {
     }
 
     @Test
-    public void english_1650_650_HomePage() {
-        eyes.open(driver,"test app", "english_1650_650_part1", new RectangleSize(1650, 650));
+    public void english_Laptop_1650_650_HomePage() {
+        eyes.open(driver,"test app", "english_Laptop_1650_650", new RectangleSize(1300, 600));
         driver.get("http://www.autoexpostore.com/");
         waitFullPageLoading();
         eyes.checkWindow("Header");
-        scrollToElement(driver,driver.findElement(By.className(".home-about")));
-        eyes.checkWindow("AboutAutoExpo");
-        scrollToElement(driver,driver.findElement(By.className(".footer-list")));
+        scrollToElement(driver,driver.findElement(By.className("home-about")));
+        eyes.checkWindow("About AutoExpo");
+        scrollToElement(driver,driver.findElement(By.className("block-name")));
+        eyes.checkWindow("Search Vehicle by Body type + Advanced Search");
+        scrollToElement(driver,driver.findElement(By.className("footer-list")));
         eyes.checkWindow("Footer");
-
-        waitFullPageLoading();
-        eyes.checkWindow("After login");
         eyes.close();
     }
 
     @Test
-    public void incorrectCredentials() {
-        eyes.open(driver,"test app", "IncorrectCredentials", new RectangleSize(1150, 650));
-        driver.get("http://www.andreyb.ixloo.com/dms");
+    public void english_GalaxyS5_360_640_HomePage() {
+        eyes.open(driver,"test app", "english_GalaxyS5_360_640_HomePage", new RectangleSize(360, 640));
+        driver.get("http://www.autoexpostore.com/");
         waitFullPageLoading();
-        eyes.checkWindow("Login");
-        driver.findElement(By.cssSelector("#login")).clear();
-        driver.findElement(By.cssSelector("#login")).sendKeys("dealerlogin");
-        driver.findElement(By.cssSelector("#password")).clear();
-        driver.findElement(By.cssSelector("#password")).sendKeys("00");
-        driver.findElement(By.cssSelector("#login2")).click();
-        waitFullPageLoading();
-        eyes.checkWindow("Logged in");
+        eyes.checkWindow("Header");
+        scrollToElement(driver,driver.findElement(By.className("home-about")));
+        eyes.checkWindow("About AutoExpo");
+        scrollToElement(driver,driver.findElement(By.className("footer-list")));
+        eyes.checkWindow("Footer");
         eyes.close();
     }
+
+    /*@Test
+    public void spanish_Laptop_1650_650_HomePage() {
+        eyes.open(driver,"test app", "spanish_Laptop_1650_650_HomePage", new RectangleSize(1300, 600));
+        driver.get("http://www.autoexpostore.com/");
+        waitFullPageLoading();
+        new Actions(driver).moveToElement(driver.findElement(By.cssSelector(".header-socials"))).moveByOffset(-1000, 0).click().perform();
+        driver.findElements(By.cssSelector(".languageName")).get(1).click();
+        waitFullPageLoading();
+        eyes.checkWindow("Header");
+        scrollToElement(driver,driver.findElement(By.className("home-about")));
+        eyes.checkWindow("AboutAutoExpo");
+        scrollToElement(driver,driver.findElement(By.className("block-name")));
+        eyes.checkWindow("Search Vehicle by Body type + Advanced Search");
+        scrollToElement(driver,driver.findElement(By.className("footer-list")));
+        eyes.checkWindow("Footer");
+        eyes.close();
+    }*/
+
+
+
+
 
     @AfterMethod
     public void teatDown() {
